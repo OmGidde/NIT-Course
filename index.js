e854f5265233d1931ecd679e1e26d14f3f04ce38
OmGidde <omgidde555@gmail.com>
2026-02-13 15:31:33 +0530

    js

---
+++ index.js
@@ -0,0 +1,28 @@
+/* console.log("hello world");
+let a;
+a = 10;
+let b ;
+b = 20;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(a - b);
+
+let age = 25;
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+} */
+
+/* var num = 60;
+if (num >= 80) {
+    console.log("First class");
+}else if (num >= 60) {
+    console.log("Second class");
+}else if (num >= 40) {
+    console.log("Third class");
+}else {
+    console.log("Fail");
+} */ 
